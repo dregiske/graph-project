867ef68c47b8baef18b68860120e7db5025c092e
Andre Giske <AndreGiske@MacBook-Air.local>
2025-05-28 18:33:31 -0700

    tests

--- Graph.hpp
+++ Graph.hpp
@@ -1,28 +1,19 @@
 #ifndef GRAPH_H
 #define GRAPH_H
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <string>
-#include <vector>
-#include <tuple>
-#include <unordered_map>
-#include <unordered_set>
-#include <queue>
-#include <algorithm>
-using namespace std;
+
+#include "commons.hpp"
 
 class Graph{
     private:
+		// undordered map for adjacency list
+		// able to hold decimals in weights
         unordered_map<string, vector<pair<string, double>>> adjacency_list;
+
         unordered_set<string> nodes_set;
 
-        void dfs(const string& node, double threshold, unordered_set<string>& visited, vector<string>& component);
-        void bfs(const string& node, double threshold, unordered_set<string>& visited, vector<string>& component);
-        bool is_connected(const string& start, const string& end, double threshold, unordered_set<string>& visited);
     public:
+		// construct a graph using csv files
         Graph(const char* const& edgelist_csv_fn);
-
 };
 
 #endif /* GRAH_H */
---
+++ commons.hpp
@@ -0,0 +1,16 @@
+#ifndef COMMONS_HPP
+#define COMMONS_HPP
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+#endif /* COMMONS_HPP */
